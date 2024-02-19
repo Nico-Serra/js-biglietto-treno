e2d7cba6c482c88d36b716f31e0bd67cc37d6974
Nico-Serra <nico.serra000@gmail.com>
2024-02-19 14:42:04 +0100

    add first step and add prompt

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+console.log('hello world');
+
+//-Individuare dall'utente i km che dovrà percorrere e la sua età
+    let trip = Number(prompt('Inserisci qui quanti km dovrai percorrere'));
+    console.log(trip);
+
+    let userAge = Number(prompt('Inserisci qui la tua età'));
+    console.log(userAge);
+
+//-In base alle informazioni trapelate calcolare il prezzo totale del viaggio tenendo conto degli sconti
+//-Mostrare il prezzo finale
