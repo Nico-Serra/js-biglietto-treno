83f9d3fc39f59b26eae966a6dc8254d33359d9f2
Nico-Serra <nico.serra000@gmail.com>
2024-02-19 15:58:22 +0100

    add if with alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,11 +9,11 @@ va applicato uno sconto del 40% per gli over 65
 */
 
 //-Individuare dall'utente i km che dovrà percorrere e la sua età
-    let trip = Number(prompt('Inserisci qui quanti km dovrai percorrere'));
-    console.log(trip);
+let trip = Number(prompt('Inserisci qui quanti km dovrai percorrere'));
+console.log(trip);
 
-    let userAge = Number(prompt('Inserisci qui la tua età'));
-    console.log(userAge);
+let userAge = Number(prompt('Inserisci qui la tua età'));
+console.log(userAge);
 
 /*-In base alle informazioni trapelate calcolare il prezzo totale del viaggio tenendo conto degli sconti
     -Moltiplicare l'età con i 0.21 €
@@ -22,18 +22,44 @@ va applicato uno sconto del 40% per gli over 65
         -ALTRIMENTI SE è Over 65 applicare il 40% di sconto
         -ALTRIMENTI non applicare nessuno sconto
 */
-    const priceKm = 0.21
-    //console.log(typeof priceKm);
+//-Moltiplicare l'età con i 0.21 €
+const priceKm = 0.21;
+//console.log(typeof priceKm);
 
-    let priceTicket = priceKm * trip
-    console.log(priceKm * trip);
+let priceTicket = priceKm * trip;
+console.log(priceKm * trip);
 
+//Sconti
+let discounted100 = priceTicket / 100;
+console.log(discounted100);
 
+let salesUnder18 = 20
 
-    let salesUnder18 = 20
+let salesOver65 = 40
 
-    let salesOver65 = 40
+console.log(typeof salesUnder18, typeof salesOver65);
 
-    console.log(typeof salesUnder18, typeof salesOver65);
+/*
+-SE l'utente è under 18 applicare il 20% di sconto
+-ALTRIMENTI SE è Over 65 applicare il 40% di sconto
+-ALTRIMENTI non applicare nessuno sconto
+*/
+
+if (userAge < 18) {
+    //console.log('Hai diritto al 20% di sconto');
+    //console.log(Number(priceTicket.toFixed(2) - (discounted100.toFixed(2) * salesUnder18)).toFixed(2))
+    alert('Hai diritto al 20% di sconto');
+    alert(`Il prezzo del biglietto è ${Number(priceTicket.toFixed(2) - (discounted100.toFixed(2) * salesUnder18)).toFixed(2)}€`)
+
+} else if (userAge > 65) {
+    //console.log('Hai diritto al 40% di sconto');
+    //console.log(Number(priceTicket.toFixed(2) - (discounted100.toFixed(2) * salesOver65)).toFixed(2))
+    alert('Hai diritto al 40% di sconto');
+    alert(`Il prezzo del biglietto è ${Number(priceTicket.toFixed(2) - (discounted100.toFixed(2) * salesOver65)).toFixed(2)}€`)
+} else {
+    //console.log('non hai diritto a nessuno sconto');
+    alert('non hai diritto a nessuno sconto');
+    alert(`Il prezzo del biglietto è ${priceTicket.toFixed(2)}€`)
+}
 
 //-Mostrare il prezzo finale
